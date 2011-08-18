fae4a2dbb965b1915c9a13f839b46ffe6f568df2
Tony Schneider <tonywok@gmail.com>
2011-08-18 03:47:37 -0400

    real close

--- roman_numerals.lisp
+++ roman_numerals.lisp
@@ -1,5 +1,5 @@
 (defparameter *numerals* (list (cons 1000 "M") (cons 500 "D") (cons 100 "C")
-                               (cons 50 "L") (cons 10 "X") (cons 5 "V")))
+                               (cons 50 "L") (cons 10 "X") (cons 5 "V") (cons 1 "I")))
 
 (defun roman-for (num)
   (cdr (assoc num *numerals*)))
@@ -8,22 +8,22 @@
   (mapcar #'car *numerals*))
 
 (defun romanify (arabic-num)
-  (labels ((add-Is (num str)
-                   (if (eq 0 num) str
-                     (add-Is (- num 1) (concatenate 'string str "I"))))
-
-           (I-before-base (str base)
-                          (concatenate 'string str "I" (roman-for base)))
+  (labels ((add-roman (num str base)
+                   (if (eq 0 num)
+                     str
+                     (add-roman (- num 1) (concatenate 'string str (roman-for base)) base)))
 
            (build (bases num str)
-                  (let* ((base (car bases))
-                         (left-over (mod num base)))
-                    (cond ((eq num 0) str) ; return str when num is 0 - done
-                      ((eq num base) ; num equals base - add base
-                        (build (cdr bases) left-over (concatenate 'string str (roman-for base))))
-                      ((eq num (- base 1)) ; num is one less than base - add I before base
-                        (build (cdr bases) left-over (I-before-base str base)))
-                      ((> num (- base 1)) ; num is more than two less than base - go a base lower
-                        (build (cdr bases) num str))
-                      (T (build (cdr bases) left-over (add-Is left-over str)))))))
+                  (let ((base (car bases)))
+                    ; find the biggest numeral within 1 of num
+                    (if (eq 0 num)                                             ; base case
+                      str
+                      (multiple-value-bind (quotient remainder) (floor num base)
+                        (cond ((and (> quotient 1) (eq remainder 0))           ; base goes in evenly
+                                (build (cdr bases) remainder (add-roman quotient str base)))
+                              ((and (> quotient 1) (not (eq remainder 0)))     ; base goes in evenly, have some left
+                                (build (cdr bases) remainder (add-roman quotient str base)))
+                              ((and (eq quotient 0) (eq remainder (- base 1))) ; wierd case (ie IV)
+                                (concatenate 'string str "I" (roman-for base)))
+                              (T (build (cdr bases) remainder str))))))))
     (build (base-numbers) arabic-num "")))
