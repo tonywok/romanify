59c949acd57df68597e17ded1a92592d449ba0ee
Tony Schneider <tonywok@gmail.com>
2011-08-18 09:15:17 -0400

    "works"

--- roman_numerals.lisp
+++ roman_numerals.lisp
@@ -1,5 +1,4 @@
-(defparameter *numerals* (list (cons 1000 "M") (cons 500 "D") (cons 100 "C")
-                               (cons 50 "L") (cons 10 "X") (cons 5 "V") (cons 1 "I")))
+(defparameter *numerals* (list (cons 1000 "M") (cons 500 "D") (cons 100 "C") (cons 50 "L") (cons 10 "X") (cons 5 "V") (cons 1 "I")))
 
 (defun roman-for (num)
   (cdr (assoc num *numerals*)))
@@ -7,23 +6,26 @@
 (defun base-numbers ()
   (mapcar #'car *numerals*))
 
+(defun add-roman (num str base)
+  (if (eq 0 num) str
+    (add-roman (- num 1) (concatenate 'string str (roman-for base)) base)))
+
 (defun romanify (arabic-num)
   (labels ((add-roman (num str base)
-                   (if (eq 0 num)
-                     str
-                     (add-roman (- num 1) (concatenate 'string str (roman-for base)) base)))
-
+                      (if (eq 0 num)
+                        str
+                        (add-roman (- num 1) (concatenate 'string str (roman-for base)) base)))
            (build (bases num str)
                   (let ((base (car bases)))
-                    ; find the biggest numeral within 1 of num
-                    (if (eq 0 num)                                             ; base case
-                      str
+                    (if (eq 0 num) str                                         ; base case
                       (multiple-value-bind (quotient remainder) (floor num base)
-                        (cond ((and (> quotient 1) (eq remainder 0))           ; base goes in evenly
+                        (cond ((and (> quotient 0) (eq remainder 0))           ; base goes in evenly, append base(s)
                                 (build (cdr bases) remainder (add-roman quotient str base)))
-                              ((and (> quotient 1) (not (eq remainder 0)))     ; base goes in evenly, have some left
+                              ((and (> quotient 0) (eq remainder (- base 1)))  ; base goes in evenly, append bases(s) & wierd leftover
+                                (concatenate 'string (add-roman quotient str base) (concatenate 'string str "I" (roman-for base))))
+                              ((and (> quotient 0) (< remainder (- base 1)))   ; base goes in evenly, handle leftover
                                 (build (cdr bases) remainder (add-roman quotient str base)))
                               ((and (eq quotient 0) (eq remainder (- base 1))) ; wierd case (ie IV)
                                 (concatenate 'string str "I" (roman-for base)))
-                              (T (build (cdr bases) remainder str))))))))
+                              (T (build (cdr bases) remainder str))))))))      ; bottom out
     (build (base-numbers) arabic-num "")))
